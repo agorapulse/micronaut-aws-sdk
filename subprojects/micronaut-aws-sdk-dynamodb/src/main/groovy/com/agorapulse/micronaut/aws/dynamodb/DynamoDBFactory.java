d433c9af588c36ec0539c9c8c38bb760bb650c36
Vladimir Orany <vladimir@orany.cz>
2020-01-07 15:02:10 +0100

    updated license, using kordamp quality plugins

--- subprojects/micronaut-aws-sdk-dynamodb/src/main/groovy/com/agorapulse/micronaut/aws/dynamodb/DynamoDBFactory.java
+++ subprojects/micronaut-aws-sdk-dynamodb/src/main/groovy/com/agorapulse/micronaut/aws/dynamodb/DynamoDBFactory.java
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
- * Copyright 2020 Vladimir Orany.
+ * Copyright 2018-2020 Agorapulse.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
