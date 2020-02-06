5be8d52e8586c8143d49c4c5a9c7e5b579e79ee7
Maxime <maxime@agorapulse.com>
2020-02-06 16:01:44 +0100

    fix typo

--- subprojects/micronaut-aws-sdk-sns/src/main/groovy/com/agorapulse/micronaut/aws/sns/SimpleNotificationServiceConfiguration.groovy
+++ subprojects/micronaut-aws-sdk-sns/src/main/groovy/com/agorapulse/micronaut/aws/sns/SimpleNotificationServiceConfiguration.groovy
@@ -50,7 +50,7 @@ abstract class SimpleNotificationServiceConfiguration extends DefaultRegionAndEn
     protected SimpleNotificationServiceConfiguration(String prefix, Environment environment) {
         this.environment = environment
         ios = forPlatform(prefix, 'ios', environment)
-        ios = forPlatform(prefix, 'iosSandbox', environment)
+        iosSandbox = forPlatform(prefix, 'iosSandbox', environment)
         android = forPlatform(prefix, 'android', environment)
         amazon = forPlatform(prefix, 'amazon', environment)
     }
