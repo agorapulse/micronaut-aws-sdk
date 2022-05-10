22d2e6cae1894697c48aa9d1c0ac39c877fda7b4
musketyr <vladimir@orany.cz>
2022-05-10 12:36:16 +0200

    added no-op remapper to support incremental annotation processing

---
+++ subprojects/micronaut-amazon-awssdk-sqs/src/main/java/com/agorapulse/micronaut/amazon/awssdk/sqs/annotation/remap/QueueClientRemapper.java
@@ -0,0 +1,26 @@
+package com.agorapulse.micronaut.amazon.awssdk.sqs.annotation.remap;
+
+import com.agorapulse.micronaut.amazon.awssdk.sqs.annotation.QueueClient;
+import io.micronaut.core.annotation.AnnotationValue;
+import io.micronaut.inject.annotation.AnnotationRemapper;
+import io.micronaut.inject.visitor.VisitorContext;
+
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.List;
+
+public class QueueClientRemapper implements AnnotationRemapper {
+
+    @Nonnull
+    @Override
+    public String getPackageName() {
+        return QueueClient.class.getPackage().getName();
+    }
+
+    @Nonnull
+    @Override
+    public List<AnnotationValue<?>> remap(AnnotationValue<?> annotation, VisitorContext visitorContext) {
+        return Collections.singletonList(annotation);
+    }
+
+}
---
+++ subprojects/micronaut-aws-sdk-dynamodb/src/main/groovy/com/agorapulse/micronaut/aws/dynamodb/annotation/remap/ServiceAnnotationRemapper.java
@@ -0,0 +1,26 @@
+package com.agorapulse.micronaut.aws.dynamodb.annotation.remap;
+
+import com.agorapulse.micronaut.aws.dynamodb.annotation.Service;
+import io.micronaut.core.annotation.AnnotationValue;
+import io.micronaut.inject.annotation.AnnotationRemapper;
+import io.micronaut.inject.visitor.VisitorContext;
+
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.List;
+
+public class ServiceAnnotationRemapper implements AnnotationRemapper {
+
+    @Nonnull
+    @Override
+    public String getPackageName() {
+        return Service.class.getPackage().getName();
+    }
+
+    @Nonnull
+    @Override
+    public List<AnnotationValue<?>> remap(AnnotationValue<?> annotation, VisitorContext visitorContext) {
+        return Collections.singletonList(annotation);
+    }
+
+}
