87cbe59c69543e1bdef07f3ce7facab7002e2986
Vladimir Orany <vladimir@orany.cz>
2022-05-10 15:41:33 +0200

    automatic annotation processing (#105)
    * added no-op remapper to support incremental annotation processing
    
    * moved remappers into separate projects
    
    * fixed license headers, added `annotationProcessor` dependencies into documentation

---
+++ subprojects/micronaut-amazon-awssdk-dynamodb-annotation-processor/src/main/java/com/agorapulse/micronaut/amazon/awssdk/dynamodb/annotation/remap/ServiceAnnotationRemapper.java
@@ -0,0 +1,42 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2018-2022 Agorapulse.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.agorapulse.micronaut.amazon.awssdk.dynamodb.annotation.remap;
+
+import io.micronaut.core.annotation.AnnotationValue;
+import io.micronaut.inject.annotation.AnnotationRemapper;
+import io.micronaut.inject.visitor.VisitorContext;
+
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.List;
+
+public class ServiceAnnotationRemapper implements AnnotationRemapper {
+
+    @Nonnull
+    @Override
+    public String getPackageName() {
+        return "com.agorapulse.micronaut.amazon.awssdk.dynamodb.annotation";
+    }
+
+    @Nonnull
+    @Override
+    public List<AnnotationValue<?>> remap(AnnotationValue<?> annotation, VisitorContext visitorContext) {
+        return Collections.singletonList(annotation);
+    }
+
+}
