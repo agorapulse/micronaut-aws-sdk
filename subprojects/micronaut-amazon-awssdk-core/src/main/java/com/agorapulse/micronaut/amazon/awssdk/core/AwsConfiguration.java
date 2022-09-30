0bb457b401d6155c123f6a267fcfe9b6bf76722f
imdadtw <110462015+imdadtw@users.noreply.github.com>
2022-09-30 16:32:40 +0100

    add containers credential provider to the credentials provider chain (#119)

--- subprojects/micronaut-amazon-awssdk-core/src/main/java/com/agorapulse/micronaut/amazon/awssdk/core/AwsConfiguration.java
+++ subprojects/micronaut-amazon-awssdk-core/src/main/java/com/agorapulse/micronaut/amazon/awssdk/core/AwsConfiguration.java
@@ -39,6 +39,7 @@ public class AwsConfiguration {
             EnvironmentVariableCredentialsProvider.create(),
             SystemPropertyCredentialsProvider.create(),
             ProfileCredentialsProvider.create(),
+            ContainerCredentialsProvider.builder().build(),
             InstanceProfileCredentialsProvider.create()
         );
     }
