a0ae90a110af1d22c46cd555c5eeb49fcdfa31a1
Vladimir Orany <vladimir@orany.cz>
2024-01-16 14:09:01 +0100

    library to provide integration testing for the AWS Lambda functions (#217)
    (cherry picked from commit 1c97e540886aa93b9a8db641f19e277a14d36863)

--- subprojects/micronaut-amazon-awssdk-lambda-integration-testing/src/main/java/com/agorapulse/micronaut/amazon/awssdk/lambda/itest/TestAwsLambdaRuntime.java
+++ subprojects/micronaut-amazon-awssdk-lambda-integration-testing/src/main/java/com/agorapulse/micronaut/amazon/awssdk/lambda/itest/TestAwsLambdaRuntime.java
@@ -35,6 +35,9 @@ import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
 
@@ -66,8 +69,10 @@ public class TestAwsLambdaRuntime {
      */
     public static InvocationRequest makeStreamRequest(InputStream input) throws IOException {
         InvocationRequest request = new InvocationRequest();
-        try (input) {
-            request.setContent(input.readAllBytes());
+        try {
+            request.setContent(readNBytes(input));
+        } finally {
+            input.close();
         }
         return request;
     }
@@ -117,7 +122,7 @@ public class TestAwsLambdaRuntime {
 
         PojoSerializer<?> serializer = EventHandlerLoader.getSerializerCached(EventHandlerLoader.Platform.UNKNOWN, extractReturnType(handlerInfo));
         ByteArrayOutputStream baos = requestHandler.call(request);
-        return (O) serializer.fromJson(baos.toString(StandardCharsets.UTF_8));
+        return (O) serializer.fromJson(new String(baos.toByteArray(), StandardCharsets.UTF_8));
 
     }
 
@@ -298,7 +303,8 @@ public class TestAwsLambdaRuntime {
         // If the returnType is not a ParameterizedType, attempt to match it against generic superclass parameters.
         if (!(returnType instanceof ParameterizedType)) {
             Type genericSuperclass = clazz.getGenericSuperclass();
-            if (genericSuperclass instanceof ParameterizedType parameterizedType) {
+            if (genericSuperclass instanceof ParameterizedType) {
+                ParameterizedType parameterizedType = (ParameterizedType) genericSuperclass;
                 Type[] typeArguments = parameterizedType.getActualTypeArguments();
                 for (Type typeArgument : typeArguments) {
                     if (Objects.requireNonNull(returnType).getTypeName().equals(typeArgument.getTypeName())) {
@@ -320,7 +326,8 @@ public class TestAwsLambdaRuntime {
 
         if (!(returnType instanceof ParameterizedType)) {
             for (Type type : clazz.getGenericInterfaces()) {
-                if (type instanceof ParameterizedType parameterizedType) {
+                if (type instanceof ParameterizedType) {
+                    ParameterizedType parameterizedType = (ParameterizedType) type;
                     Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
                     Type[] typeArguments = ((Class<?>)parameterizedType.getRawType()).getTypeParameters();
                     for (int i = 0; i < typeArguments.length; i++) {
@@ -346,6 +353,71 @@ public class TestAwsLambdaRuntime {
             clazz = clazz.getSuperclass(); // Move up in the class hierarchy.
         }
         return null; // Method not found in class hierarchy.
+    }
+
+    // backport from newer Java
+    private static byte[] readNBytes(InputStream is) throws IOException {
+        if (Integer.MAX_VALUE < 0) {
+            throw new IllegalArgumentException("len < 0");
+        }
+
+        List<byte[]> bufs = null;
+        byte[] result = null;
+        int total = 0;
+        int remaining = Integer.MAX_VALUE;
+        int n;
+        do {
+            byte[] buf = new byte[Math.min(remaining, 8192)];
+            int nread = 0;
+
+            // read to EOF which may read more or less than buffer size
+            while ((n = is.read(buf, nread,
+                Math.min(buf.length - nread, remaining))) > 0) {
+                nread += n;
+                remaining -= n;
+            }
+
+            if (nread > 0) {
+                if (Integer.MAX_VALUE - 8 - total < nread) {
+                    throw new OutOfMemoryError("Required array size too large");
+                }
+                if (nread < buf.length) {
+                    buf = Arrays.copyOfRange(buf, 0, nread);
+                }
+                total += nread;
+                if (result == null) {
+                    result = buf;
+                } else {
+                    if (bufs == null) {
+                        bufs = new ArrayList<>();
+                        bufs.add(result);
+                    }
+                    bufs.add(buf);
+                }
+            }
+            // if the last call to read returned -1 or the number of bytes
+            // requested have been read then break
+        } while (n >= 0 && remaining > 0);
+
+        if (bufs == null) {
+            if (result == null) {
+                return new byte[0];
+            }
+            return result.length == total ?
+                result : Arrays.copyOf(result, total);
+        }
+
+        result = new byte[total];
+        int offset = 0;
+        remaining = total;
+        for (byte[] b : bufs) {
+            int count = Math.min(b.length, remaining);
+            System.arraycopy(b, 0, result, offset, count);
+            offset += count;
+            remaining -= count;
+        }
+
+        return result;
     }
 
 
