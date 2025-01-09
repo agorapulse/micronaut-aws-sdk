179b060ff0ba39f5572875120982671c3915efa1
musketyr <vladimir@orany.cz>
2025-01-09 14:49:43 +0100

    fixed license headers for 2025

--- subprojects/micronaut-amazon-awssdk-kinesis/src/main/java/com/agorapulse/micronaut/amazon/awssdk/kinesis/DefaultKinesisService.java
+++ subprojects/micronaut-amazon-awssdk-kinesis/src/main/java/com/agorapulse/micronaut/amazon/awssdk/kinesis/DefaultKinesisService.java
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
- * Copyright 2018-2024 Agorapulse.
+ * Copyright 2018-2025 Agorapulse.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
